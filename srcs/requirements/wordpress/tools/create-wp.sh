1b2d72eb4e06ce8be190578bf099e2bfb054035f
Ahmet Karaca <ahmet_karaca@outlook.com.tr>
2023-08-01 01:43:40 +0300

    update

--- srcs/requirements/wordpress/tools/create-wp.sh
+++ srcs/requirements/wordpress/tools/create-wp.sh
@@ -2,11 +2,12 @@
 # https://www.serverkaka.com/2018/12/install-and-configure-latest-wordpress-ubuntu.html
 
 # Check if wp-config.php exist.
-if [ -f ./wp-config.php ]
+if [-f ./wp-config.php]
 then
 	echo "wordpress already downloaded!"
 else
 
+	rm -rf wp-admin wp-content wp-includes
 
 	#Download wordpress and all config file
 	wget http://wordpress.org/latest.tar.gz
@@ -21,6 +22,12 @@ else
 	sed -i "s/localhost/$MYSQL_HOSTNAME/g" wp-config-sample.php
 	sed -i "s/database_name_here/$MYSQL_DATABASE/g" wp-config-sample.php
 	cp wp-config-sample.php wp-config.php
+
+	# WordPress'in çekirdek kurulumunu ve kullanıcıları oluşturmayı gerçekleştiriyor.
+	echo "wordpress creating users..."
+	wp core install --allow-root --url=${WP_URL} --title=${WP_TITLE} --admin_user=${WP_ADMIN_LOGIN} --admin_password=${WP_ADMIN_PASSWORD} --admin_email=${WP_ADMIN_EMAIL};
+	wp user create --allow-root ${WP_USER_LOGIN} ${WP_USER_EMAIL} --user_pass=${WP_USER_PASSWORD};
+	echo "wordpress set up!"
 
 fi
 
